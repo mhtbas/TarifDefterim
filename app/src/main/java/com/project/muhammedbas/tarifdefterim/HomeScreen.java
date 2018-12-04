5017578e31fda80d15f5e062f0869022c6d2d7cb
mhtbas <mht.bas@hotmail.com>
2018-12-04 14:16:47 +0300

    Home design

--- app/src/main/java/com/project/muhammedbas/tarifdefterim/HomeScreen.java
+++ app/src/main/java/com/project/muhammedbas/tarifdefterim/HomeScreen.java
@@ -1,6 +1,7 @@
 package com.project.muhammedbas.tarifdefterim;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -8,18 +9,46 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.TextView;
 import android.widget.ToggleButton;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class HomeScreen extends AppCompatActivity {
 
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
+
+    private TextView usernameText;
+    private DatabaseReference mUserDatabase;
+    private String currentUser;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_screen);
 
         init();
+
+        mUserDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                String username=dataSnapshot.child("name").getValue().toString();
+                usernameText.setText(username+"'in tarifleri");
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
     @Override
@@ -60,6 +89,12 @@ public class HomeScreen extends AppCompatActivity {
         mToggle.syncState();
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        usernameText=findViewById(R.id.username);
+
+        currentUser=FirebaseAuth.getInstance().getCurrentUser().getUid();
+        mUserDatabase=FirebaseDatabase.getInstance().getReference().child("users").child(currentUser);
 
     }
 }
