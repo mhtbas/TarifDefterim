50fc8a4100b17460f2483e8382752747902d0b89
Your Name <muhammedbas@stu.aydin.edu.tr>
2018-12-10 22:42:53 +0300

    Search screen

--- app/src/main/java/com/project/muhammedbas/tarifdefterim/SearchScreen.java
+++ app/src/main/java/com/project/muhammedbas/tarifdefterim/SearchScreen.java
@@ -1,13 +1,62 @@
 package com.project.muhammedbas.tarifdefterim;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class SearchScreen extends AppCompatActivity {
+
+    private EditText searchEdit;
+    private ImageView searchbutton;
+    private String searchString;
+    private RecyclerView searchRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.search_screen);
+        
+        init();
+
+
+
+
     }
+
+    private void init() {
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setTitle("Tarif Ara");
+
+        searchEdit=findViewById(R.id.search_field);
+        searchbutton=findViewById(R.id.search_btn);
+
+        searchRecyclerView=findViewById(R.id.searchlist);
+        searchRecyclerView.setHasFixedSize(true);
+        searchRecyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+
+            case android.R.id.home:
+                this.finish();
+                return true;
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    
+
 }
